c4ed7676c3bacd9e0b0bb2889be9775aea360a1e
SirKira(!Uzumaki) <48526201+sirKiraUzumaki@users.noreply.github.com>
2021-04-20 21:29:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const { request, response } = require("express");
+const express = require("express");
+const app = express();
+
+
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port http://localhost:" + listener.address().port);
+});
